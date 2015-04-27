2a6e58f4401b9ebed8698abcd3fec4d26b1c30ff
SupremeWei <supremewei@SupremeWeideMacBook-Pro.local>
2015-04-27 22:44:54 +0800

    fix icon and descriptions

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -34,42 +34,40 @@
     <div id="wrapper">
         <div id="featured-wrapper">
             <div id="featured" class="container">
-                <div class="column1"> <span class="icon icon-cogs"></span>
+                <div class="column1"> <span class="icon icon-play"></span>
                     <div class="title">
-                        <h2>Maecenas lectus sapien</h2>
+                        <h2>系列搜尋</h2>
                     </div>
-                    <p>In posuere eleifend odio. Quisque semper augue mattis wisi. Pellentesque viverra vulputate enim. Aliquam erat volutpat.</p>
+                    <p>Series</p>
                 </div>
-                <div class="column2"> <span class="icon icon-legal"></span>
+                <div class="column2"> <span class="icon icon-search"></span>
                     <div class="title">
-                        <h2>Praesent scelerisque</h2>
+                        <h2>料號搜尋</h2>
                     </div>
-                    <p>In posuere eleifend odio. Quisque semper augue mattis wisi. Pellentesque viverra vulputate enim. Aliquam erat volutpat.</p>
+                    <p>Part No.</p>
                 </div>
-                <div class="column3"> <span class="icon icon-unlock"></span>
+                <div class="column3"> <span class="icon icon-search"></span>
                     <div class="title">
-                        <h2>Fusce ultrices fringilla</h2>
+                        <h2>規格說明</h2>
                     </div>
-                    <p>In posuere eleifend odio. Quisque semper augue mattis wisi. Pellentesque viverra vulputate enim. Aliquam erat volutpat.</p>
+                    <p>Description</p>
                 </div>
-                <div class="column4"> <span class="icon icon-wrench"></span>
+                <div class="column4"> <span class="icon icon-search"></span>
                     <div class="title">
-                        <h2>Etiam posuere augue</h2>
+                        <h2>未知</h2>
                     </div>
-                    <p>In posuere eleifend odio. Quisque semper augue mattis wisi. Pellentesque viverra vulputate enim. Aliquam erat volutpat.</p>
+                    <p>Don't Know put in.</p>
                 </div>
             </div>
         </div>
         <div id="extra" class="container">
-            <h2>Maecenas vitae orci vitae tellus feugiat eleifend</h2>
-            <span>Quisque dictum integer nisl risus, sagittis convallis, rutrum id, congue, and nibh</span>
-            <p>This is <strong>Breadth</strong>, a free, fully standards-compliant CSS template designed by <a href="http://templated.co" rel="nofollow">TEMPLATED</a>. The photos in this template are from <a href="http://fotogrph.com/"> Fotogrph</a>. This free template is released under the <a href="http://templated.co/license">Creative Commons Attribution</a> license, so you're pretty much free to do whatever you want with it (even use it commercially) provided you give us credit for it. Have fun :) </p>
+            <h2>三執業執照通過</h2>
 
-            <a href="#" class="button">Etiam posuere</a> </div>
+            <a href="#" class="button">產品資訊</a> </div>
     </div>
 
     <div id="copyright" class="container">
-        <p>&copy; Untitled. All rights reserved. | Photos by <a href="http://fotogrph.com/">Fotogrph</a> | Design by <a href="http://templated.co" rel="nofollow">TEMPLATED</a>.</p>
+        <p>S.C.R. Cooperation</p>
     </div>
 
 	<!-- Scripts -->
