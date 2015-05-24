1e7113e5e9b2878aeddc972d02bc8683e6344380
SupremeWei <supremewei@SupremeWeideMacBook-Pro.local>
2015-05-24 23:38:40 +0800

    Fix owl params

--- public/js/product.js
+++ public/js/product.js
@@ -1,4 +1,15 @@
 $(document).ready(function() {
-    $("#owl-demo").owlCarousel();
+    var owl = $("#owl-product");
 
+    owl.owlCarousel({
+        autoPlay: true,
+        slideSpeed : 300,
+        paginationSpeed : 400,
+        stopOnHover: true,
+        navigation : true,
+        navigationText: ["前一個", "後一個"],
+        pagination: true,
+        mouseDrag: true,
+        touchDrag: true
+    });
 });
