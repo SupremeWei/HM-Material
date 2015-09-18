32dcc260e6b36fe812ca0561c13824a9fe89e227
Supreme <supremewei@ifalo.com.tw>
2015-09-18 16:25:34 +0800

    Fix product root page right side display

--- resources/views/productIndex.blade.php
+++ resources/views/productIndex.blade.php
@@ -2,10 +2,28 @@
 
 @section('itemInformation')
     <div class="col-md-9" name="productContent">
-        <div class="headline-center">
+        <div class="heading heading-v1 margin-bottom-20">
             <h2>請先選擇左側項目</h2>
 
             <p>翰鎂公司致力於提供完善的產品</p>
+            <div class="center-block row content-boxes-v2 content-boxes-v2-o margin-bottom-40">
+                <div class="col-sm-6 sm-margin-bottom-40">
+                    <h2 class="heading-sm overflow-h">
+                        <i class="icon-custom icon-bg-red fa fa-bullhorn"></i>
+                        <span>發光二極體 <small>多項產品</small></span>
+                    </h2>
+                    <hr class="no-margin margin-bottom-10">
+                    <p>插件式發光二極體, 貼片型發光二極體, 七段顯示器, HOUSING</p>
+                </div>
+                <div class="col-sm-6 sm-margin-bottom-40">
+                    <h2 class="heading-sm overflow-h">
+                        <i class="icon-custom icon-bg-blue fa fa-bullhorn"></i>
+                        <span>DC-Use <small>多項產品</small></span>
+                    </h2>
+                    <hr class="no-margin margin-bottom-10">
+                    <p>Metallized Polyester Film, Metallized Polypropylene Film, Polyester Film, Polypropylene Film</p>
+                </div>
+            </div>
         </div>
     </div>
 @endsection
