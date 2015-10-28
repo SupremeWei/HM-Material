e9f8305a995cf459f135e015fa3449b4bc1fea46
SupremeWei <supremewei@SupremeWeideMacBook-Pro.local>
2015-10-28 23:20:35 +0800

    Add dataseed to example login

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+use App\Eloquent\User;
+
+class UserTableSeeder extends Seeder {
+
+	/**
+	 * For admin seed
+	 *
+	 * @return void
+	 */
+	public function run()
+	{
+		User::create(array(
+			'name'     => 'supreme',
+			'username' => 'supreme',
+			'email'    => 'supremewei@gmail.com',
+			'password' => Hash::make('awesome99'),
+		));
+
+		User::create(array(
+			'name'     => 'supreme',
+			'username' => 'supreme',
+			'email'    => 'supremewei@gmail.com',
+			'password' => Hash::make('awesome99'),
+		));
+	}
+
+}
